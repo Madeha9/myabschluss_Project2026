a6920db3c11f02e4ebfe10918079ee2423e69548
Madeha Mohammed <madehamohammed36cs@gmail.com>
2026-03-05 19:02:57 +0100

    feat(Class): Create ErrorCode as enum Class to handle the Error Messages

---
+++ src/main/java/at/madeha/intelliinvoice/exception/ErrorCode.java
@@ -0,0 +1,11 @@
+package at.madeha.intelliinvoice.exception;
+
+/*
+enum are constant file that we use to save constant options to avoid the user entering mistakes
+ */
+public enum ErrorCode {
+    INVOICE_NOT_FOUND,
+    INVALID_INVOICE_DATA,
+    FILE_UPLOAD_FAILED,
+    DATABASE_ERROR
+}
