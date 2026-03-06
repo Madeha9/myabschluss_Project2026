4168f27e705e3af88038d1e54801401cff6e378a
Madeha Mohammed <madehamohammed36cs@gmail.com>
2026-03-06 18:42:23 +0100

    feat(Service): add method to the interface StorageService

--- src/main/java/at/madeha/intelliinvoice/service/StorageService.java
+++ src/main/java/at/madeha/intelliinvoice/service/StorageService.java
@@ -1,7 +1,15 @@
+
 package at.madeha.intelliinvoice.service;
+
+
+import org.jboss.resteasy.reactive.multipart.FileUpload;
+
 
 /*
  * Defines methods for file storage
  */
 public interface StorageService {
+    String uploadFile(FileUpload uploadInvoice);
+
+
 }
