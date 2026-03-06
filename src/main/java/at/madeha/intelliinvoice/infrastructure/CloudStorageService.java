f0064bc1272312eaa0b521a717d8a6769a3adfc8
Madeha Mohammed <madehamohammed36cs@gmail.com>
2026-03-06 18:39:08 +0100

    feat(infrastructure): add initial Methods (UploadFile) to the  CloudStorage Class

--- src/main/java/at/madeha/intelliinvoice/infrastructure/CloudStorageService.java
+++ src/main/java/at/madeha/intelliinvoice/infrastructure/CloudStorageService.java
@@ -1,6 +1,20 @@
+
 package at.madeha.intelliinvoice.infrastructure;
 
-import at.madeha.intelliinvoice.service.StorageService;
 
+import at.madeha.intelliinvoice.service.StorageService;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.jboss.resteasy.reactive.multipart.FileUpload;
+
+
+@ApplicationScoped
+//so that Quarkus can create the object for this class using the ID concept .
 public class CloudStorageService implements StorageService {
+
+
+    @Override
+    public String uploadFile(FileUpload uploadInvoice) {
+        // write the  cloud upload logic later on
+        return "https://cloud.example.com/" + uploadInvoice.fileName(); // placeholder
+    }
 }
